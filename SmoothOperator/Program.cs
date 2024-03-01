b54c03b9a8998ad1063bbe14180c4d080623ed87
“[Breno <manancialgd@gmail.com>
2024-03-01 17:47:16 +0000

    Added the project SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static byte n;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um número inteiro não-negativo: ");
+
+            if (byte.TryParse(Console.ReadLine(), out byte userInput))
+            {
+                n = userInput;
+
+                n = (byte)(n - 1);
+                Console.WriteLine(n);
+
+                n = (byte)(n + 1);
+                Console.WriteLine(n);
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Certifique-se de inserir um número inteiro não-negativo.");
+            }
+        }
+    }
+}
