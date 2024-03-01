761752631ef632d1d78f4040b7bd0d5218e5a5d4
“[Breno <manancialgd@gmail.com>
2024-03-01 20:26:28 +0000

    Exercise 5 done

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -4,9 +4,44 @@ namespace Cilindro
 {
     class Program
     {
+        static float r;
+        static float a;
+        static float V;
+        static float Sa;
+
         static void Main(string[] args)
         {
-            Console.Write("Hello, World!");
+            Console.Write("Enter the radius of a cylinder: ");
+            string inputTMP = Console.ReadLine();
+
+            if (float.TryParse(inputTMP, out float radius))
+            {
+                r = radius;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for radius.");
+                return;
+            }
+
+            Console.Write("Enter the height of a cylinder: ");
+            inputTMP = Console.ReadLine();
+
+            if (float.TryParse(inputTMP, out float altura))
+            {
+                a = altura;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for height.");
+                return;
+            }
+
+            V = MathF.PI * r * r * a;
+            Console.WriteLine("The Volume of your Cylinder is: {0:F2}", V);
+
+            Sa = 2f * MathF.PI * r * (r + a);
+            Console.WriteLine("The Surface Area of your Cylinder is: {0:F2}", Sa);
         }
     }
 }
