2c5e02339f6ac98a0be4c4242838fb0f503e592f
“[Breno <manancialgd@gmail.com>
2024-03-01 17:56:51 +0000

    Added Cilindro project

---
+++ Cilindro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, World!");
+        }
+    }
+}
