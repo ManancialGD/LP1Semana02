f8eb619cf9ab26de973253712c07eaf3c90e4882
“[Breno <manancialgd@gmail.com>
2024-03-01 17:51:57 +0000

    made changes for exercise 4

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -14,11 +14,29 @@ namespace SmoothOperator
             {
                 n = userInput;
 
+                // Operação de decremento
                 n = (byte)(n - 1);
-                Console.WriteLine(n);
+                Console.WriteLine($"Resultado da operação de decremento de n: {n}");
 
+                // Operação de incremento
                 n = (byte)(n + 1);
-                Console.WriteLine(n);
+                Console.WriteLine($"Resultado da operação de incremento de n: {n}");
+
+                // Operação de divisão por 2
+                n = (byte)(n / 2);
+                Console.WriteLine($"Resultado da operação de divisão por 2 de n: {n}");
+
+                // Operação de shift left por 3
+                n = (byte)(n << 3);
+                Console.WriteLine($"Resultado da operação de shift left por 3 de n: {n}");
+
+                // Operação de XOR com 5
+                n = (byte)(n ^ 5);
+                Console.WriteLine($"Resultado da operação de XOR com 5 de n: {n}");
+
+                // Verifica se é maior que 10
+                bool isMaiorQue10 = n > 10;
+                Console.WriteLine($"É maior que 10? {isMaiorQue10}");
             }
             else
             {
