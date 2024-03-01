eae143013e87ec87595f45d6881c3645868cd17e
“[Breno <manancialgd@gmail.com>
2024-03-01 21:21:22 +0000

    Apropriada

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int maxTurns = 20;
+            int turn = 0;
+            while (turn >= maxTurns)
+            {
+                turn++;
+
+                // Skips current cycle if current turn is divisible by 3
+                if (turn % 3 == 0)
+                {
+                    continue;
+                }
+                else if (turn == 10)
+                {
+                    continue;
+                }
+
+                // Print turn number
+                Console.WriteLine($"Turn number {turn}");
+            }
+
+            // Print number of last turn
+            Console.WriteLine($"Last turn was {turn}");
+        }
+    }
+}
