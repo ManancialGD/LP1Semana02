c3a6eefb2661d7428206efbb0a6f23b20df9f12b
“[Breno <manancialgd@gmail.com>
2024-03-01 21:37:23 +0000

    Apropriada

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static string question, response;
+        static bool isDone = false;
+        static void Main(string[] args)
+        {
+
+            
+            while (!isDone)
+            {
+                Console.Write("Ask your question: ");
+                question = Console.ReadLine();
+                switch(question)
+                {
+                    case "EXIT":
+                        isDone = true;
+                        break;
+                    default:
+                        response = "Speak english, alien";
+                        Console.WriteLine(response);
+                        break;
+                }
+
+
+            }
+        }
+    }
+}
