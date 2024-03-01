ff61f90df9c6a3a9f4de77dbfada991c1d4c6c9f
“[Breno <manancialgd@gmail.com>
2024-03-01 20:58:07 +0000

    Apropriada

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Integer: Min = {int.MinValue}, Max = {int.MaxValue}");
+            Console.WriteLine($"Unsigned Integer: Min = {uint.MinValue}, Max = {uint.MaxValue}");
+            Console.WriteLine($"Long: Min = {long.MinValue}, Max = {long.MaxValue}");
+            Console.WriteLine($"Unsigned Long: Min = {ulong.MinValue}, Max = {ulong.MaxValue}");
+            Console.WriteLine($"Short: Min = {short.MinValue}, Max = {short.MaxValue}");
+            Console.WriteLine($"Unsigned Short: Min = {ushort.MinValue}, Max = {ushort.MaxValue}");
+            Console.WriteLine($"Float: Min = {float.MinValue}, Max = {float.MaxValue}");
+            Console.WriteLine($"Double: Min = {double.MinValue}, Max = {double.MaxValue}");
+            Console.WriteLine($"Decimal: Min = {decimal.MinValue}, Max = {decimal.MaxValue}");
+        }
+    }
+}
